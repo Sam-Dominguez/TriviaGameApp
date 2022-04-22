714d65e3d4954a5c5512e676a460273b258268eb
Sam-Dominguez <samdominguez@gmail.com>
2022-04-22 09:19:57 -0400

    Initial Commit

---
+++ TriviaGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TriviaGame
+//
+//  Created by Sam Dominguez on 4/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Sam Dominguez on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
